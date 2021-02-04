e58af75342dbb537e7b84b81a972f5949ea5e9ea
Jay-Mark <jpascua3@alaska.edu>
2021-02-04 02:25:34 -0900

    Time It II: Added findVector() function; finds random word

--- Hw1/Time It I/Time2.cpp
+++ Hw1/Time It I/Time2.cpp
@@ -18,7 +18,6 @@ std::vector<std::string> bookMakeVector(std::ifstream& book) { // Read file's wo
         v.push_back(word);
 
         if (book.eof()) {
-            std::cout << "Finished Reading" << std::endl;
             Timer.Stop(Timer);
             book.close();
             break;
@@ -28,19 +27,55 @@ std::vector<std::string> bookMakeVector(std::ifstream& book) { // Read file's wo
     return v;
 }
 
+void vectorFind(std::vector<std::string>& v) { // Find random number in vector
+    StopWatch Timer;
+
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count(); // Random number generator
+    std::mt19937 randNum(seed);
+    int rNum = randNum() % v.size() + 1;
+
+    std::string findWord = v[rNum];
+
+    std::cout << "FINDING: " << findWord << std::endl;
+    Timer.Start();
+    std::vector<std::string>::iterator it = std::find(v.begin(), v.end(), findWord);
+    if (it != v.end()) {
+        std::cout << findWord << " found" << std::endl;
+        Timer.Stop(Timer);
+    }
+    else
+        std::cout << "Word not found" << std::endl;
+}
+
 void gutenbergProject() {
     std::ifstream book;
+    std::vector<std::string> v;
 
     std::cout << "READING INTO VECTOR" << std::endl;
+
+    std::cout << "\nBOOK 1" << std::endl;
     book.open("Book1.txt");
-    bookMakeVector(book);
+    v = bookMakeVector(book);
+    vectorFind(v);
+
+    std::cout << "\nBOOK 2" << std::endl;
     book.open("Book2.txt");
-    bookMakeVector(book);
+    v = bookMakeVector(book);
+    vectorFind(v);
+
+    std::cout << "\nBOOK 3" << std::endl;
     book.open("Book3.txt");
-    bookMakeVector(book);
+    v = bookMakeVector(book);
+    vectorFind(v);
+
+    std::cout << "\nBOOK 4" << std::endl;
     book.open("Book4.txt");
-    bookMakeVector(book);
+    v = bookMakeVector(book);
+    vectorFind(v);
+
+    std::cout << "\nBOOK 5" << std::endl;
     book.open("Book5.txt");
-    bookMakeVector(book);
+    v = bookMakeVector(book);
+    vectorFind(v);
 
 }
