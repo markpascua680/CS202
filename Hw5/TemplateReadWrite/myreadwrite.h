8b36c3412e34c680d1228c14d1aa02b79fff12b5
Jay-Mark <jpascua3@alaska.edu>
2021-04-20 11:11:23 -0800

    MyReadWrite: Added myRead function

--- Hw5/TemplateReadWrite/myreadwrite.h
+++ Hw5/TemplateReadWrite/myreadwrite.h
@@ -4,8 +4,13 @@
 #include <fstream>
 
 template <typename Type>
-void myWrite(std::ofstream& ofile, Type num) {
+void myWrite(std::ofstream& ofile, const Type& num) {
 	ofile.write(reinterpret_cast<const char*>(&num), sizeof(num));
+}
+
+template <typename Type>
+void myRead(std::ifstream& ifile, Type& num) {
+	ifile.read(reinterpret_cast<char*>(&num), sizeof(num));
 }
 
 #endif // !MYREADWRITE_H
